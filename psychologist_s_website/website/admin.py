3433deed40567d258a9e4af9ae014a8cf410af69
kostyazu1985-arch <kostyazu1985@gmail.com>
2026-09-02 22:29:52 +0300

    diplom

--- psychologist_s_website/website/admin.py
+++ psychologist_s_website/website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Appointment
 
 # Register your models here.
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'telegram', 'created_at')
--- psychologist_s_website/website/models.py
+++ psychologist_s_website/website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Appointment(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Имя")
+    telegram = models.CharField(max_length=100, verbose_name="Ник в Telegram")
+    client_request = models.TextField(verbose_name="Ваш запрос")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата заявки")
+
+    def __str__(self):
+        return f"Заявка от {self.name}"
